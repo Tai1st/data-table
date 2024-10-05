07591e2bc6b25a51c9cbd43b652f253581ffd43a
Tai1st <109854568+Tai1st@users.noreply.github.com>
2024-10-05 11:37:16 +0700

    remove import
    remove import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";

--- src/components/blog-table/Table/columns.tsx
+++ src/components/blog-table/Table/columns.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import { Button } from "@/components/ui/button";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 import { ColumnDef } from "@tanstack/react-table"
 import { ArrowUpDown, MoreHorizontal } from "lucide-react";
 
